92af6a8ea55b5c362b79a2d20b606800bb828a0d
AdrianaNeagu <adriana.neagu86@gmail.com>
2018-01-20 14:09:57 +0200

    All is up and running

--- app/src/main/java/com/example/android/moviequizapp/MainActivity.java
+++ app/src/main/java/com/example/android/moviequizapp/MainActivity.java
@@ -7,14 +7,15 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     static int totalPoints;
     RadioButton questionOneAnswerThree;
     RadioButton questionTwoAnswerOne;
-    CheckBox questionThreeAnswerTrue;
-    CheckBox questionFourAnswerTrue;
+    RadioButton questionThreeAnswerTrue;
+    RadioButton questionFourAnswerTrue;
     EditText questionFiveEditText;
     CheckBox questionSixAnswerOne;
     CheckBox questionSixAnswerFour;
@@ -26,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
         questionOneAnswerThree = findViewById(R.id.questionOneAnswerThree_radio);
         questionTwoAnswerOne = findViewById(R.id.questionTwoAnswerOne_radio);
-        questionThreeAnswerTrue = findViewById(R.id.questionThreeAnswerTrue_checkbox);
-        questionFourAnswerTrue = findViewById(R.id.questionFourAnswerTrue_checkbox);
+        questionThreeAnswerTrue = findViewById(R.id.questionThreeAnswerTrue_radio);
+        questionFourAnswerTrue = findViewById(R.id.questionFourAnswerTrue_radio);
         questionFiveEditText = findViewById(R.id.questionFive_editText);
         questionSixAnswerOne = findViewById(R.id.questionSixAnswerOne_checkbox);
         questionSixAnswerFour = findViewById(R.id.questionSixAnswerFour_checkbox);
@@ -46,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         if(questionFourAnswerTrue.isChecked()) {
             totalPoints += 1;
         }
-        String questionFiveString = questionFiveEditText.toString();
+        String questionFiveString = questionFiveEditText.getText().toString();
         if(questionFiveString.equals("Gotham")) {
             totalPoints += 1;
         }
@@ -54,7 +55,23 @@ public class MainActivity extends AppCompatActivity {
             totalPoints += 1;
         }
 
-        
+        String messageZero = getResources().getString(R.string.toast0);
+        String lessPoints = getResources().getString(R.string.toast1);
+        String messageLess = String.format(lessPoints, totalPoints);
+        String mediumPoints = getResources().getString(R.string.toast2);
+        String messageMedium = String.format(mediumPoints, totalPoints);
+        String highPoints = getResources().getString(R.string.toast3);
+        String messageHigh = String.format(highPoints, totalPoints);
+
+        if(totalPoints == 0) {
+            Toast.makeText(this, messageZero, Toast.LENGTH_LONG).show();
+        } else if(totalPoints == 1 || totalPoints == 2) {
+            Toast.makeText(this, messageLess, Toast.LENGTH_LONG).show();
+        } else if(totalPoints == 3 || totalPoints == 4) {
+            Toast.makeText(this, messageMedium, Toast.LENGTH_LONG).show();
+        } else if(totalPoints == 5 || totalPoints == 6) {
+            Toast.makeText(this, messageHigh, Toast.LENGTH_LONG).show();
+        }
     }
 
     public void resetAnswers(View v) {
